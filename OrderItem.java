dfe4622ed9d555616ccc8df31c62691f81893fc1
juanksparta117 <90666698+juanksparta117@users.noreply.github.com>
2025-10-05 19:50:44 -0500

    Update OrderItem.java
    se vuelven a subir archivos debido a error

---
+++ OrderItem.java
@@ -0,0 +1,25 @@
+package com.example.proyecto.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantity;
+    private Double price;
+    private Double discount = 0.0;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    // getters y setters
+}
