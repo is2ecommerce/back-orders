8df8c2be770b876a7131207ec0b189b2b3f9e2d9
juanksparta117 <90666698+juanksparta117@users.noreply.github.com>
2025-10-05 19:51:40 -0500

    Update Product.java
    se vuelven a subir archivos debido a error

---
+++ Product.java
@@ -0,0 +1,17 @@
+package com.example.proyecto.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Double price;
+    private Integer stock;
+
+    // getters y setters
+}
